e098dc7dc11bb7810070384e370746450ddca2c5
Victor Kaugesaar <kaugesaar@gmail.com>
2015-10-29 11:13:50 +0100

    added CampaignEndDate, simple way to find campaign which have ended

---
+++ Scripts/Campaigns/CampaignEndDate.js
@@ -0,0 +1,28 @@
+/**
+ *  CampaignEndDate - find or filter out
+ *  campaign which are enabled but have ended.
+ *
+ *  https://groups.google.com/forum/#!topic/adwords-scripts/Xnn3jR65YcI
+ */
+
+function main() {
+    var campaignIterator = AdWordsApp.campaigns()
+        .withCondition('Status = "ENABLED"')
+        .get();
+
+    while(campaignIterator.hasNext()) {
+        var campaign = campaignIterator.next();
+        var endDate = campaign.getEndDate();
+      
+        if(endDate != null && DATE.year > endDate.year && DATE.month > endDate.month && DATE.day > endDate.day) { continue; }
+      
+        // From here you can do what you want with campaigns which hasen't ended.
+
+    }
+
+}
+
+var DATE = (function () {
+    var d = new Date();
+    return { year: d.getFullYear(), month: d.getMonth() + 1, day: d.getDate()}
+})();
