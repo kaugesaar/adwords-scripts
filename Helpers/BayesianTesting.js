c1bd9ca16ee7dbd13808f2383b4fd3114c4dd809
Victor Kaugesaar <kaugesaar@gmail.com>
2016-01-11 15:17:39 +0100

    added formulas for count data

--- Helpers/BayesianTesting.js
+++ Helpers/BayesianTesting.js
@@ -1,7 +1,8 @@
 var BayesianTesting = function() {
 
     /**
-     * The propability B beats A in a binary-outcome test. Based on
+     * The propability B beats A in a binary-outcome test
+     * (eg. conversion rate). Based on
      * http://www.evanmiller.org/bayesian-ab-testing.html
      * 
      * @param  {int} alphaA number of success for A
@@ -27,7 +28,8 @@ var BayesianTesting = function() {
     }
 
     /**
-     * The propability C beats A and B in a binary-outcome test.
+     * The propability C beats A and B in a 
+     * binary-outcome test (eg. conversion rate).
      * 
      * @param  {int} alphaA number of success for A
      * @param  {int} betaA  number of failures for A
@@ -60,6 +62,58 @@ var BayesianTesting = function() {
                  + total;
     }
 
+    /**
+     * Function for analyzing count data. Returns the propability
+     * that group 1 has a higher arrivale rate than group 2.
+     * http://www.evanmiller.org/bayesian-ab-testing.html#count_ab
+     * 
+     * @param  {int} alpha1 total events for group one
+     * @param  {int} beta1  total exposure for group one
+     * @param  {int} alpha2 total events for group two
+     * @param  {int} beta2  total exposure for group two
+     * @return {int}
+     */
+    this.propability_1_beats_2 = function(alpha1,beta1,alpha2,beta2) {
+        var total = 0.0;
+
+        for(var k = 0; k < alpha1-1; k++) {
+            total += Math.exp(k * Math.log(beta1) + alpha2 * Math.log(beta2)
+                - (k + alpha2) * Math.log(beta1 + beta2)
+                - Math.log(k + beta2) - lbeta(k + 1, alpha2))
+        }
+
+        return total;
+    }
+
+    /**
+     * Function for analyzing count data. Returns the propability
+     * that group 1 has a higher arrivale rate than group 2 and group 3.
+     * http://www.evanmiller.org/bayesian-ab-testing.html#count_ab
+     * 
+     * @param  {int} alpha1 total events for group one
+     * @param  {int} beta1  total exposure for group one
+     * @param  {int} alpha2 total events for group two
+     * @param  {int} beta2  total exposure for group two
+     * @param  {int} alpha3 total events for group three
+     * @param  {int} beta3  total exposure for group three
+     * @return {int}
+     */
+    this.propability_1_beats_2_and_3 = function(alpha1,beta1,alpha2,beta2,alpha3,beta3) {
+        var total = 0.0;
+
+        for(var k = 0; k < alpha2-1; k++) {
+            for(var l = 0; l < alpha3-1; l++) {
+                total += Math.exp(alpha1 * Math.log(beta1) + k * Math.log(beta2) + 1 * Math.log(beta3)
+                    - (k+l+beta1) * Math.log(beta1 + beta2 + beta3)
+                    + lgamma(k+l+alpha1) - lgamma(k+1) - lgamma(l+1) - lgamma(alpha1))
+            }
+        }
+
+        return 1 - this.propability_1_beats_2(alpha2,beta2,alpha1,beta1)
+                 - this.propability_1_beats_2(alpha3,beta3,alpha1,beta1)
+                 + total;
+    }
+
     var lgamma = function(x) {
         var j = 0;
         var cof = [
@@ -80,3 +134,7 @@ var BayesianTesting = function() {
     }
 
 }
+
+var bt = new BayesianTesting();
+console.log(bt.propability_1_beats_2(10,100,10,50))
+console.log(bt.propability_1_beats_2_and_3(10,100,10,150,5,75))
